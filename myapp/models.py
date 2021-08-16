2533402b56eff24eeb43c4cf49f2f0e839a708d2
Rextremistz <37291863+Rextremistz@users.noreply.github.com>
2021-08-17 00:33:12 +0800

    basic done

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,13 @@ from django.db import models
 
 
 class Player(models.Model):
-    name = models.CharField(max_length=100)
-    team = models.CharField(max_length=100)
-    status = models.CharField(max_length=100, default='Healthy')
-    lastseasonttl = models.FloatField(default=0)
+    name = models.CharField('Name', max_length=100)
+    team = models.CharField('Team', max_length=100)
+    season = models.CharField(max_length=100, blank=True, null=True)
+    games = models.IntegerField(blank=True, null=True)
+    points = models.FloatField(max_length=100, blank=True, null=True)
+    average = models.FloatField(max_length=100, blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
