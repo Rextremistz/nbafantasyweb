0b271e2a4a77a6df44becc66f1a2dcafc6885fa4
Rextremistz <37291863+Rextremistz@users.noreply.github.com>
2021-08-21 11:03:11 +0800

    added column search

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,63 @@
 $(document).ready(function () {
-  $('#example').DataTable({
-    "ajax" : {
-      "url" : "./static/data/fantasy.json",
-      "dataSrc" : "players"
+  var table = $('#mtable').DataTable({
+    "ajax": {
+      "url": "./static/data/fantasy.json",
+      "dataSrc": "players"
     },
 
-    'columns' : [
-      {'data':'name'},
-      {'data':'y1_points'},
-      {'data':'y2_points'},
-      {'data':'y3_points'},
-      {'data':'y1_average'},
-      {'data':'y2_average'},
-      {'data':'y3_average'},
-      {'data':'y1_games'},
-      {'data':'y2_games'},
-      {'data':'y3_games'},
-      {'data':'y1_price'},
-      {'data':'y2_price'},
-      {'data':'y3_price'},
-    ]
+    "lengthMenu": [[20, 50, 100, -1], [20, 50, 100, "All"]],
 
+    'columns': [
+      { 'data': 'name' },
+      { 'data': 'y1_points' },
+      { 'data': 'y2_points' },
+      { 'data': 'y3_points' },
+      { 'data': 'y1_average' },
+      { 'data': 'y2_average' },
+      { 'data': 'y3_average' },
+      { 'data': 'y1_games' },
+      { 'data': 'y2_games' },
+      { 'data': 'y3_games' },
+      { 'data': 'y1_price' },
+      { 'data': 'y2_price' },
+      { 'data': 'y3_price' },
+    ],
 
+    stateSave: true
   });
-} );
+
+  initComplete: (function () {
+    var r = $('#mtable tfoot tr');
+    r.find('th').each(function () {
+      $(this).css('padding', 8);
+    });
+    $('#mtable thead').append(r);
+    $(this).css('text-align', 'center');
+  });
+
+  $('#mtable thead th').each(function () {
+    var title = $(this).text();
+    $(this).html('<input type="text"/>'); //placeholder = "Search ' + title + '"
+  });
+
+  table.columns().every(function () {
+    var that = this;
+    $('input', this.header()).on('keyup change', function () {
+      if (that.search() !== this.value) {
+        that
+          .search(this.value)
+          .draw();
+      }
+
+
+    });
+
+    $('input', this.header()).on('click', function (e) { //stop sorting order
+      e.stopPropagation();
+    });
+  });
+
+
+
+
+});
