b99e13358117aa53363c625610a35aca9c10a007
Rextremistz <37291863+Rextremistz@users.noreply.github.com>
2021-10-13 12:21:37 +0800

    Update settings.py

--- nbafantasyweb/settings.py
+++ nbafantasyweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fi@+vsu5tn9fgf(6f@%6vkxt39)xv(6_el6$^g+ev@8z^b0x6-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'nbafantasyweb.herokuapp.com']
 
 
 # Application definition
