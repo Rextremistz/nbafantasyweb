31271a6e819e05c9e5af23d3522b1a0ee57356e0
Rextremistz <37291863+Rextremistz@users.noreply.github.com>
2021-10-13 12:56:28 +0800

    Update settings.py

--- nbafantasyweb/settings.py
+++ nbafantasyweb/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if sys.argv[1] != 'runserver':
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = os.path.join(BASE_DIR, 'var', "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
