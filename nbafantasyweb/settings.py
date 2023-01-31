64ce85214ec1426478fe4dc1fe49226376c996e8
Yen Ju Andrew Chiang <37291863+chiangyenju@users.noreply.github.com>
2023-01-31 19:49:11 +0800

    static

--- nbafantasyweb/settings.py
+++ nbafantasyweb/settings.py
@@ -126,9 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-if sys.argv[1] != 'runserver':
-    STATIC_ROOT = os.path.join(BASE_DIR, 'var', "static")
+# if sys.argv[1] != 'runserver':
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'var', "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
